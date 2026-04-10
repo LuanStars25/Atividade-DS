e74ee747ddd98153cfa3641f74c1a4f4218c34dc
LuanZeichan <luan.russi.machado@escola.pr.gov.br>
2026-04-10 09:44:49 -0300

    Add files via upload

---
+++ ex19.py
@@ -0,0 +1,7 @@
+# ex19 Prioridade
+idade = int(input("Idade: "))
+gestante = input("é gestante (sim ou não): ")
+if idade >= 60 or gestante == "sim":
+    print("Senha preferencial")
+else:
+    print("Atendimento normal")
